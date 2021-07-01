b591132c903bbfcc3b2b86f4c37078293d65ebd5
open-yaming <wumingquan@open-yamingdeiMac-Pro.local>
2021-07-02 02:21:59 +0800

    添加打包仓库脚本

---
+++ build.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+name="OpenCore-"
+srcFile="EFI"
+srcFile1="EFI"
+srcPath="./OpenCore/V1版本U用"
+srcPath1="./OpenCore/V2版本U用"
+targetPath="./build"
+version=0.7.2-beta1-V
+cpu=1版本
+cpu1=2版本
+if [ -d $srcPath/$srcFile ];then
+echo "打包目录存在"
+if [ -d $srcPath1/$srcFile1 ];then
+echo "打包目录存在"
+if [ -d $targetPath ];then
+tar -zcvf  $targetPath/$name$version$cpu.tar.gz --exclude=.DS_Store $srcPath/$srcFile
+if [ -d $targetPath ];then
+tar -zcvf  $targetPath/$name$version$cpu1.tar.gz --exclude=.DS_Store $srcPath1/$srcFile1
+else
+echo "打包目录不存在"
+fi
+else
+echo "存放目录不存在"
+fi
+fi
+fi
