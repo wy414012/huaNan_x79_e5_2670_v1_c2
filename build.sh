f3300a1922bf8ac1f54682492391ee3be7eb08f4
open-yaming <wumingquan@open-yamingdeiMac-Pro.local>
2021-07-02 09:50:21 +0800

    update build.sh

--- build.sh
+++ build.sh
@@ -6,22 +6,23 @@ srcFile1="EFI"
 srcPath="./OpenCore/V1版本U用"
 srcPath1="./OpenCore/V2版本U用"
 targetPath="./build"
-version=0.7.2-beta1-V
-cpu=1版本
-cpu1=2版本
+version=0.7.2-beta-V
+date=$(date +%Y%m%d)
+cpu=1-
+cpu1=2-
 if [ -d $srcPath/$srcFile ];then
 echo "打包目录存在"
 if [ -d $srcPath1/$srcFile1 ];then
 echo "打包目录存在"
 if [ -d $targetPath ];then
-tar -zcvf  $targetPath/$name$version$cpu.tar.gz --exclude=.DS_Store $srcPath/$srcFile
+tar -zcvf  $targetPath/$name$version$cpu$date.tar.gz --exclude=.DS_Store $srcPath/$srcFile
 if [ -d $targetPath ];then
-tar -zcvf  $targetPath/$name$version$cpu1.tar.gz --exclude=.DS_Store $srcPath1/$srcFile1
+tar -zcvf  $targetPath/$name$version$cpu1$date.tar.gz --exclude=.DS_Store $srcPath1/$srcFile1
 echo "打包完成开始生成sha256sum码"
 if [ -d $targetPath ];then
-sha256sum $targetPath/$name$version$cpu.tar.gz >$targetPath/$name$version$cpu.tar.gz.sha256sum.txt
+sha256sum $targetPath/$name$version$cpu$date.tar.gz >$targetPath/$name$version$cpu$date.tar.gz.sha256sum.txt
 if [ -d $targetPath ];then
-sha256sum $targetPath/$name$version$cpu1.tar.gz >$targetPath/$name$version$cpu1.tar.gz.sha256sum.txt
+sha256sum $targetPath/$name$version$cpu1$date.tar.gz >$targetPath/$name$version$cpu1$date.tar.gz.sha256sum.txt
 else
 echo "打包目录不存在"
 fi
