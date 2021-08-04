efce6b58ef5dde732b62676ccdde4699df3d8e98
峨眉山市雅铭网络工作室 <417733808@qq.com>
2021-08-04 14:29:25 +0800

    升级核心与相关驱动到稳定版本

--- build.sh
+++ build.sh
@@ -20,9 +20,9 @@ if [ -d $targetPath ];then
 tar -zcvf  $targetPath/$name$version$cpu1$date.tar.gz --exclude=.DS_Store $srcPath1/$srcFile1
 echo "打包完成开始生成sha256sum码"
 if [ -d $targetPath ];then
-sha256sum $targetPath/$name$version$cpu$date.tar.gz >$targetPath/$name$version$cpu$date.tar.gz.sha256sum.txt
+md5 $targetPath/$name$version$cpu$date.tar.gz >$targetPath/$name$version$cpu$date.tar.gz.sha256sum.txt
 if [ -d $targetPath ];then
-sha256sum $targetPath/$name$version$cpu1$date.tar.gz >$targetPath/$name$version$cpu1$date.tar.gz.sha256sum.txt
+md5 $targetPath/$name$version$cpu1$date.tar.gz >$targetPath/$name$version$cpu1$date.tar.gz.sha256sum.txt
 else
 echo "打包目录不存在"
 fi
