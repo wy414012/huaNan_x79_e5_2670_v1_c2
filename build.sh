7ca5fd47d28ebcc3da943488bbeac2127025b0ed
Follow the wind <36281137+wy414012@users.noreply.github.com>
2023-03-29 17:37:17 +0800

    Update build.sh
    更新打包脚本，便于理解

--- build.sh
+++ build.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
 name="OpenCore-"
-srcFile="/"
-srcPath="./OpenCore"
-targetPath="./build"
-version=0.8.9
+src_file="/OpenCore"
+src_path="./OpenCore"
+target_path="./build"
+version="0.8.9"
 date=$(date +%Y%m%d)
-cpu=-REL-
+cpu="-REL"
 
-if [ -d $srcPath/$srcFile ];then
-echo "打包目录存在"
+if [ -d "$src_path/$src_file" ]; then
+    echo "打包目录存在"
+    if [ -d "$target_path" ]; then
+        tar -zcvf "$target_path/$name$version$cpu$date.tar.gz" --exclude=.DS_Store "$src_path/$src_file"
+    else
+        echo "打包目录不存在"
+    fi
+    printf "打包完成，开始生成 sha256sum 码\n"
 
-if [ -d $targetPath ];then
-tar -zcvf  $targetPath/$name$version$cpu$date.tar.gz --exclude=.DS_Store $srcPath/$srcFile
-else
-echo "打包目录不存在"
-fi
-echo "打包完成开始生成sha256sum码"
-fi
-if [ -d $targetPath ];then
-md5sum $targetPath/$name$version$cpu$date.tar.gz >$targetPath/$name$version$cpu$date.tar.gz.sha256sum.txt
-else
-echo "存放目录不存在"
+    if [ -d "$target_path" ]; then
+        md5sum "$target_path/$name$version$cpu$date.tar.gz" > "$target_path/$name$version$cpu$date.tar.gz.sha256sum.txt"
+    else
+        echo "存放目录不存在"
+    fi
 fi
