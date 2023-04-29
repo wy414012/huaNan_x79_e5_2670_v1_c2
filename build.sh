cc160f39200f2adf059327a0450aff68f1578df6
YamingNetwork <ymwl@ip6.cool>
2023-04-30 00:25:16 +0800

    更新构建脚本去除日期命名

--- build.sh
+++ build.sh
@@ -5,20 +5,18 @@ src_file="/"
 src_path="./OpenCore"
 target_path="./build"
 version=$(grep OPEN_CORE_VERSION ./VERsion | sed 's/.*"\(.*\)".*/\1/' | grep -E '^[0-9.]+$')
-date=$(date +%Y%m%d)
-cpu="-REL"
 
 if [ -d "$src_path/$src_file" ]; then
     printf "打包目录存在"
     if [ -d "$target_path" ]; then
-        tar -zcvf "$target_path/$name$version$cpu$date.tar.gz" --exclude=.DS_Store "$src_path/$src_file"
+        tar -zcvf "$target_path/$name$version-REL.tar.gz" --exclude=.DS_Store "$src_path/$src_file"
     else
         printf "打包目录不存在"
     fi
     printf "打包完成，开始生成 sha256sum 码\n"
 
     if [ -d "$target_path" ]; then
-        md5sum "$target_path/$name$version$cpu$date.tar.gz" > "$target_path/$name$version$cpu$date.tar.gz.sha256sum.txt"
+        md5sum "$target_path/$name$version-REL.tar.gz" > "$target_path/$name$version-REL.tar.gz.sha256sum.txt"
     else
         printf "存放目录不存在"
     fi
